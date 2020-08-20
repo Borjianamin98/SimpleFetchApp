4a9a363cad764fb53f5a63e8bfd81067c9be964e
Amin Borjian <borjianamin98@outlook.com>
2020-08-20 15:44:26 +0430

    Review the program at the end

--- src/App.js
+++ src/App.js
@@ -24,6 +24,24 @@ const SORTS = {
     POINTS: list => sortBy(list, 'points').reverse(),
 };
 
+const updateSearchTopStoriesState = (hits, page) => (prevState) => {
+    const { searchKey, results } = prevState;
+    const oldHits = results && results[searchKey]
+        ? results[searchKey].hits
+        : [];
+    const updatedHits = [
+        ...oldHits,
+        ...hits
+    ];
+    return {
+        results: {
+            ...results,
+            [searchKey]: { hits: updatedHits, page }
+        },
+        isLoading: false
+    };
+};
+
 class App extends Component {
     _isMounted = false;
 
@@ -51,24 +69,7 @@ class App extends Component {
 
     setSearchTopStories(result) {
         const {hits, page} = result;
-        const {searchKey, results} = this.state;
-        const oldHits = results && results[searchKey]
-            ? results[searchKey].hits
-            : [];
-        const updatedHits = [
-            ...oldHits,
-            ...hits
-        ];
-        this.setState({
-            results: {
-                ...results,
-                [searchKey]: {
-                    hits: updatedHits,
-                    page
-                }
-            },
-            isLoading: false
-        });
+        this.setState(updateSearchTopStoriesState(hits, page));
     }
 
     fetchSearchTopStories(searchTerm, page = 0) {
